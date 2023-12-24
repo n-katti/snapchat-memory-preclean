0fa70714dcb7a9a7a91933caea52dfa25c47dfa0
Nikhil <nikhilkatti@outlook.com>
2023-12-24 04:02:32 -0500

    git ignore

--- main.py
+++ main.py
@@ -7,24 +7,23 @@ import os
 
 # Sets one location for the parent folder of the current script where photos from Snapchat Memory Downloader are placed in yearly folders
 location = Path(__file__)
-location_parent = location.parent
-os.chdir(location_parent)
+location_parent = location.parent / 'input'
 
 # Set another path for the memories that were downloaded separately (as part of the caption overlay)
 input = Path(r'C:\Users\nikhi\OneDrive\Documents\Python Projects\snapchat-memory-overlay\input')
 
 def main():
     # Renames files downloaded directly from Snapchat to remove prefixed dates
-    rf.rename_without_date_prefix(input / 'memories')
+    # rf.rename_without_date_prefix(input / 'memories')
 
     # For files downloaded through Snapchat Memory Downloader, this renames/renumbers the files
-    # df = rd.get_all_pics(location_parent=location_parent)
-    # df = rd.renumber_files(df=df, location_parent=location_parent)
+    df = rd.get_all_pics(location_parent=location_parent)
+    df = rd.renumber_files(df=df, location_parent=location_parent)
 
-    # tqdm.pandas(desc="Renaming Files", unit="row")
-    # df.progress_apply(lambda row: rd.rename_files(row, location_parent=location_parent), axis=1)
+    tqdm.pandas(desc="Renaming Files", unit="row")
+    df.progress_apply(lambda row: rd.rename_files(row, location_parent=location_parent), axis=1)
 
-    eon.output_json_with_only_overlays(input=input)
+    # eon.output_json_with_only_overlays(input=input)
 
 if __name__ == "__main__":
     main()
